e366d121ea5b6881e56d4c9ff8692bfb94d10db2
Bikash Sharma <36207673+bikashxsharma@users.noreply.github.com>
2019-10-23 14:56:29 +0300

    tip-calculator-done
    tip-calculator done, there can be more adjustment with css and some hide and show id, but for now this is all good.

--- calculate.js
+++ calculate.js
@@ -8,30 +8,31 @@ function calcuateTip(){
 let totalAmount=document.getElementById("total-amount").value;
 let totalpeople=document.getElementById("total-people").value;
 let tipAmount=document.getElementById("tip-amount").value;
-let showResult=document.querySelector("show-result");
+let showResult=document.querySelector(".show-result");
     //check if input value are empty
-        if(totalAmount==0 || totalpeople==0){
+        if(totalAmount === "" || totalpeople == 0){
             alert("Please enter values");
             return;
          }
     // if number of input is empty or 0 then assign value to 1
-    if(totalpeople==="" || totalpeople <=0){
-        totalpeople=1;
+    if(totalpeople === "" || totalpeople <= 0){
+        totalpeople = 1;
 
     }
 
-   // update showresult with Tip amount
-    showResult.innerHTML=`Tip Amount <br/>
-    $ ${totalTip}<br/>
-    Each`; 
+   
     // tip calculation
     let totalTip= ((tipAmount/100)*totalAmount)/totalpeople;
-    return totalTip;
+    totalTipl=Math.round(totalTip*100)/100;
+    totalTip = totalTip.toFixed(2);
+
+    // update showresult with Tip amount
+    showResult.innerHTML=`Tip Amount <br/>${totalTip}<br/>Each`; 
      
 }
 // call the function on button click
-document.getElementById("calculate").addEventListener("click", function(){
+document.getElementById("calculate").onclick = function(){
     calcuateTip();
-})
+};
 
 
